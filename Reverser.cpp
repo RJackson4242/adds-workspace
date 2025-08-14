81a63eb77b950ff8b37ea0145ef16bcba9117a9e
RJackson4242 <a1968360@adelaide.edu.au>
2025-08-14 10:36:39 +0930

    first commit

---
+++ Reverser.cpp
@@ -0,0 +1,25 @@
+#include "Reverser.h"
+
+int Reverser::reverseDigit(int value)
+{
+    return reverseDigit(value, 0);
+}
+
+int Reverser::reverseDigit(int value, int acc)
+{
+    if (value == 0) { return acc; }
+    return reverseDigit(value / 10, acc * 10 + value % 10);
+}
+
+string Reverser::reverseString(string characters)
+{
+    reverseString(characters, "");
+}
+
+string Reverser::reverseString(string characters, string acc)
+{
+    if (characters.length() == 0) { return acc; }
+    acc += characters.back();
+    characters.pop_back();
+    return reverseString(characters, acc);
+}
---
+++ Reverser.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Reverser
+{
+    int reverseDigit(int value);
+    string reverseString(string characters);
+
+private:
+    int reverseDigit(int value, int acc);
+    string reverseString(string characters, string acc);
+};
