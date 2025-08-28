761ac3fc090455b20472c6ef626f0668737745e1
RJackson4242 <a1968360@adelaide.edu.au>
2025-08-28 13:30:27 +0930

    removed substring tracking

--- Finder.cpp
+++ Finder.cpp
@@ -6,11 +6,9 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
 
     vector<int> result(s2.size(), -1);
     size_t start = 0;
-    string substring;
 
     for(size_t i = 1; i <= s2.size(); i++) {
-        substring.push_back(s2[i-1]);
-        size_t found = s1.find(substring, start);
+        size_t found = s1.find(s2.substr(0, i), start);
         if (found != string::npos) {
             start = found;
             result[i-1] = found;
