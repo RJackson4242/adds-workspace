156e2e454d439452a081801eb7c164adf9b10623
RJackson4242 <a1968360@adelaide.edu.au>
2025-10-21 12:58:31 +1030

    init commit

---
+++ DocumentManager.cpp
@@ -0,0 +1,37 @@
+#include "DocumentManager.h"
+
+void DocumentManager::addDocument(string name, int id, int license_limit)
+{
+    Document doc = {id, name, license_limit};
+    documentsByID[id] = doc;
+    nameToID[name] = id;
+    borrowed[id] = unordered_set<int>();
+}
+
+void DocumentManager::addPatron(int patronID)
+{
+    patrons.insert(patronID);
+}
+
+int DocumentManager::search(string name)
+{
+    int id = nameToID[name];
+    if (id) { return id; } else { return 0; }
+}
+
+bool DocumentManager::borrowDocument(int docid, int patronID)
+{
+    if (!patrons.count(patronID)) { return false; }
+    if (!documentsByID.count(docid)) { return false; }
+    unordered_set<int> borrowers = borrowed[docid];
+    int max = documentsByID[docid].license_limit;
+    if (borrowers.size() >= max) { return false; }
+    borrowers.insert(patronID);
+    return true;
+}
+
+void DocumentManager::returnDocument(int docid, int patronID)
+{
+    if (!documentsByID.count(docid)) { return; }
+    borrowed[docid].erase(patronID);
+}
---
+++ DocumentManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+using std::string, std::unordered_map, std::unordered_set;
+
+class DocumentManager
+{
+private:
+    // Document data
+    struct Document
+    {
+        int id;
+        std::string name;
+        int license_limit;
+    };
+
+    unordered_map<int, Document> documentsByID;           // docid → Document
+    unordered_map<std::string, int> nameToID;             // name → docid
+    unordered_set<int> patrons;                           // patronID
+    unordered_map<int, std::unordered_set<int>> borrowed; // docid → set of patronIDs
+public:
+    void addDocument(string name, int id, int license_limit);
+
+    void addPatron(int patronID);
+
+    int search(string name); // returns docid if name is in the document collection or 0 if the name is not in the collection
+
+    bool borrowDocument(int docid, int patronID); // returns true if document is borrowed, false if it can not be borrowed (invalid patronid or the number of copies current borrowed has reached the license limit)
+
+    void returnDocument(int docid, int patronID);
+};
